06588f056dbe96cee24c13064e65af4188c28a83
fbosaas-cyber <fbosaas@gmail.com>
2025-09-30 19:39:59 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  basePath: '/Tyler-s-Website',
+  assetPrefix: '/Tyler-s-Website/',
+  images: {
+    unoptimized: true
+  },
+  output: 'export'
 }
 
 module.exports = nextConfig
